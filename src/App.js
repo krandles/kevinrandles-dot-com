215765550847bd70f0d5a41dc76a7e92b6183c9c
krandles <krandles@gmail.com>
2018-05-09 17:15:41 -0400

    added contact icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Kevin Randles</h1>
+        <div className="contact-logos-container">
+          <div>
+            <a href="https://github.com/krandles" target="_blank" rel="noopener noreferrer" >
+              <img className="contact-icons" src="./images/github.svg" alt=""/>
+            </a>
+          </div>
+          <div>
+            <a href="mailto:krandles@gmail.com" target="_blank" rel="noopener noreferrer" >
+              <img className="contact-icons" src="./images/gmail.svg" alt=""/>
+            </a>
+          </div>
+          <div>
+            <a href="https://linkedin.com/in/krandles" target="_blank" rel="noopener noreferrer" >
+              <img className="contact-icons" src="./images/linkedin.svg" alt=""/>
+            </a>
+          </div>
+          <div>
+            <a href="https://medium.com/@krandles" target="_blank" rel="noopener noreferrer" >
+              <img className="contact-icons" src="./images/medium.svg" alt=""/>
+            </a>
+          </div>
+          <div>
+            <a href="https://twitter.com/krandles" target="_blank"  >
+              <img className="contact-icons" src="./images/twitter.svg" alt=""/>
+            </a>
+          </div>
+        </div>
+        <p>Not much to see here yet...</p>
       </div>
     );
   }
