002dc58d907f88be11fbf3b2c0b7db814bbe9894
krandles <krandles@gmail.com>
2018-05-30 00:46:52 -0400

    added css for projects mobile layout

--- src/components/ContentAbout.js
+++ src/components/ContentAbout.js
@@ -8,7 +8,7 @@ const ContentAbout = () => (
         A computer <del>geek</del> enthusiast since the days when RAM was measured in KB, I&#39;ve spent most of my career working in IT support, but when, in Autumn 2017, I found myself with the opportunity to attend Flatiron School&#39;s web development immersive, I decided it was time for a recalibration.
       </p>
       <p>
-        Now, with a head full of web dev knowledge, over a decade of professional experience in tech, and a lifetime spent solving puzzles, shooting troubles, and taking things apart to figure out how they work (they <i>usually</i> still work when I put them back together), I&#39;m currently seeking opportunities/challlenges/creative fulfillment.
+        Now, with a head full of web dev knowledge, over a decade of professional experience in tech, and a lifetime spent solving puzzles, shooting troubles, and taking things apart to figure out how they work (they <i>usually</i> still work when I put them back together), I&#39;m currently seeking opportunities / challlenges / creative fulfillment.
       </p>
       <p>
         Interested in working with me? <a href="mailto:krandles@gmail.com" target="_blank" rel="noopener noreferrer">Get in touch</a> and let&#39;s build something incredible.
