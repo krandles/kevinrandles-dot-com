c9ae50cb47df267951d6b9176cbebf044eb61aba
krandles <krandles@gmail.com>
2018-06-06 22:50:41 -0400

    updated about page content

--- src/components/ContentAbout.js
+++ src/components/ContentAbout.js
@@ -5,10 +5,7 @@ const ContentAbout = () => (
     <div className="about">
       <h2>About Me</h2>
       <p>
-        A computer <del>geek</del> enthusiast since the day my parents brought home a C-64, I&#39;ve spent most of my career working in IT support, but found myself wanting something more fulfilling. When I found myself with the opportunity to attend Flatiron School&#39;s web development immersive, I decided it was time to stop fixing things all day long and start making things.
-      </p>
-      <p>
-        I recently finished school, where I studied Ruby, Rails, JavaScript (ES6), and React, and with over a decade of professional experience in IT, and a lifetime spent solving puzzles, shooting troubles, and taking things apart to figure out how they work, I&#39;m getting started on the next phase of my career.
+        Full-stack web developer based in Brooklyn, NY. Experienced with JavaScript(ES6), React, and Ruby on Rails, I&#39;m passionate about software that simplifies and enhances people&#39;s lives and jobs.
       </p>
       <p>
         Interested in working with me? I&#39;m currently seeking new opportunities, <a href="mailto:krandles@gmail.com" target="_blank" rel="noopener noreferrer">get in touch with me</a> and let&#39;s build something incredible.
