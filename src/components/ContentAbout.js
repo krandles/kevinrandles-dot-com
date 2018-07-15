6549305fa44bb0333076d53f0c030ba460c0a551
krandles <krandles@gmail.com>
2018-07-15 14:45:08 -0400

    switch main layout from grid to flex

--- src/components/ContentAbout.js
+++ src/components/ContentAbout.js
@@ -5,10 +5,10 @@ const ContentAbout = () => (
     <div className="about">
       <h2>About Me</h2>
       <p>
-        Full-stack web developer based in Brooklyn, NY. Experienced with JavaScript(ES6), React, and Ruby on Rails, I&#39;m passionate about software that simplifies and enhances people&#39;s lives and jobs.
+        Full-stack web developer based in Brooklyn, NY. Experienced with JavaScript (ES6), React, and Ruby on Rails, I&#39;m passionate about software that simplifies and enhances people&#39;s lives and jobs.
       </p>
       <p>
-        Interested in working with me? I&#39;m currently seeking new opportunities, <a href="mailto:krandles@gmail.com" target="_blank" rel="noopener noreferrer">get in touch with me</a> and let&#39;s build something incredible.
+        Interested in working with me? I&#39;m seeking new opportunities, <a href="mailto:krandles@gmail.com" target="_blank" rel="noopener noreferrer">get in touch with me</a> and let&#39;s build something incredible.
       </p>
     </div>
   </div>
