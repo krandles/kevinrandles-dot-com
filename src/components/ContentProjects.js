174ad8cb4ae7a7b090e7762fed03e310df9771ac
krandles <krandles@gmail.com>
2018-07-16 13:12:53 -0400

    refactor theme switch functions, moved theme colors and project data to their own files

--- src/components/ContentProjects.js
+++ src/components/ContentProjects.js
@@ -1,44 +1,6 @@
 import React from 'react';
 import ProjectDetails from './ProjectDetails';
-
-const projects = {
-  tapped: {
-    githubLink: 'https://github.com/krandles/bottle-share-frontend',
-    hostedLink: 'http://tapped.kevinrandles.com',
-    hostedLinkText: 'tapped.kevinrandles.com',
-    description: 'A multi-purpose app for craft beer enthusiasts, Tapped features beer & brewery listings/reviews and craft beer-related event listings. Users can create and manage public or private events, discuss and post photos of events, and contribute to beer reviews/listings.',
-    tech: ['React', 'Redux', 'React Router', 'Semantic UI', 'Rails', 'PostgreSQL'],
-    date: 'March 2018 (ongoing)',
-    images: ['images/tapped1.png']
-  },
-  tileMatcher: {
-    githubLink: 'https://github.com/krandles/Tile-Matcher-Frontendd',
-    hostedLink: 'http://tile-matcher.kevinrandles.com',
-    hostedLinkText: 'tile-matcher.kevinrandles.com',
-    description: 'A Concentration-style tile-matching game built with vanilla JavaScript, with support for multiple tilesets loaded from a Rails API.',
-    tech: ['JavaScript', 'Rails', 'HTML', 'CSS'],
-    date: 'February 2018',
-    images: ['images/tile-matcher1.png']
-  },
-  getErDone: {
-    githubLink: 'https://github.com/krandles/get-er-done',
-    hostedLink: 'http://geterdone.kevinrandles.com',
-    hostedLinkText: 'get-er-done.kevinrandles.com',
-    description: "A todo-list app incorporating gamification elements, Get 'Er Done allows users to manage tasks by project or category, earning experience points towards levelling up when tasks or projects are completed.",
-    tech: ['Ruby', 'Rails', 'Bootstrap'],
-    date: 'February 2018',
-    images: ['images/geterdone1.png']
-  },
-  mediocreReads: {
-    githubLink: 'https://github.com/krandles/mediocre-reads-cli',
-    hostedLink: '',
-    hostedLinkText: '',
-    description: "A Ruby CLI app that interfaces with the Goodreads API, giving users the ability to search authors and books, and list books they've reviewed.",
-    tech: ['Ruby', 'Nokogiri', 'Sinatra', 'SQLite'],
-    date: 'January 2018',
-    images: []
-  }
-};
+import projects from '../data/projects';
 
 const ContentProjects = props => (
   <div className="main-content">
---
+++ src/data/themes.js
@@ -0,0 +1,18 @@
+const themes = {
+  light: {
+    'background-color': 'rgb(255,254,200)',
+    'text-color': 'rgb(96,144,114)',
+    'link-color': 'rgb(188,75,45)',
+    'accent-color': 'rgb(75,12,11)',
+    'shadow-color': 'rgba(96,144,114,0.6)'
+  },
+  dark: {
+    'background-color': 'rgb(40,40,40)',
+    'text-color': 'rgb(96,144,114)',
+    'link-color': 'rgb(188,75,45)',
+    'accent-color': 'rgb(255,254,200)',
+    'shadow-color': 'rgba(96,144,114,0.6)'
+  }
+};
+
+export default themes;
