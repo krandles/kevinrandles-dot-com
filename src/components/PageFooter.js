922dc3b9545d794f83aad596ffbbc3d24bcac17e
krandles <krandles@gmail.com>
2018-07-17 14:33:36 -0400

    move footer and theme picker to components, add themes

---
+++ src/components/PageFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactIcons from './ContactIcons';
+import ThemePicker from './ThemePicker';
+
+const PageFooter = () => (
+  <div className="footer">
+    <div className="placeholder" />
+    <ContactIcons />
+    <ThemePicker />
+  </div>
+);
+
+export default PageFooter;
--- src/data/themes.js
+++ src/data/themes.js
@@ -1,18 +1,46 @@
 const themes = {
-  light: {
+  rez: {
     'background-color': 'rgb(255,254,200)',
     'text-color': 'rgb(96,144,114)',
     'link-color': 'rgb(188,75,45)',
     'accent-color': 'rgb(75,12,11)',
     'shadow-color': 'rgba(96,144,114,0.6)'
   },
-  dark: {
-    'background-color': 'rgb(40,40,40)',
+  'eve-1': {
+    'background-color': 'rgb(29,47,56)',
     'text-color': 'rgb(96,144,114)',
     'link-color': 'rgb(188,75,45)',
     'accent-color': 'rgb(255,254,200)',
     'shadow-color': 'rgba(96,144,114,0.6)'
-  }
+  },
+  'eve-4': {
+    'background-color': 'rgb(149,160,177)',
+    'text-color': 'rgb(22,48,71)',
+    'link-color': 'rgb(186,216,195)',
+    'accent-color': 'rgb(241,255,250)',
+    'shadow-color': 'rgba(22,48,71,0.6)'
+  },
+  'eve-6': {
+    'background-color': 'rgb(170,172,161)',
+    'text-color': 'rgb(99,113,100)',
+    'link-color': 'rgb(152,113,121)',
+    'accent-color': 'rgb(241,247,238)',
+    'shadow-color': 'rgba(99,113,100,0.6)'
+  },
+  'eve-7': {
+    'background-color': 'rgb(104,106,95)',
+    'text-color': 'rgb(189,152,69)',
+    'link-color': 'rgb(1,36,2)',
+    'accent-color': 'rgb(50,3,31)',
+    'shadow-color': 'rgba(189,152,69,0.6)'
+  },
+  'eve-8': {
+    'background-color': 'rgb(68,112,137)',
+    'accent-color': 'rgb(238,158,174)',
+    'link-color': 'rgb(163,146,226)',
+    'text-color': 'rgb(194,178,180)',
+    'shadow-color': 'rgba(238,158,174,0.6)'
+  },
 };
 
 export default themes;
